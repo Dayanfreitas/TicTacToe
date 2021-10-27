8a3500ae89aa316b4cee13f820156b6180ff96ac
Dayan Freitas <37721793+Dayanfreitas@users.noreply.github.com>
2021-10-26 22:11:26 -0300

    Criando biblioteca

---
+++ display.c
@@ -0,0 +1,21 @@
+#include "display.h"
+#include <stdio.h>
+
+void presentationScreen() {
+	// system("cls");
+	printf("\n");
+	printf("\t\t*********************************************\n");
+	printf("\t\t**              JOGO DA VELHA              **\n");
+	printf("\t\t**                                         **\n");
+    printf("\t\t*********************************************\n");
+    printf("\t\t**  /*O jogo da velha eh uma atividade     **\n");
+    printf("\t\t**   da faculdade, feita no dia 06/06/2019 **\n");
+   	printf("\t\t**   pelo estudante Dayan O. De Freitas*/  **\n");
+	printf("\t\t*********************************************\n");
+	printf("\t\t*********************************************\n");
+    
+}
+
+// int soma() {
+// 	return 1;
+// }
---
+++ display.h
@@ -0,0 +1,8 @@
+#ifndef DISPLAY_H_INCLUDED
+#define DISPLAY_H_INCLUDED
+
+#define VERSION 'V.1.1.0'
+void presentationScreen();
+
+
+#endif //DISPLAY_H_INCLUDED
