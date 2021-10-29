766af85131b1acc2c7b5613d312d26689f9d2759
Dayan Freitas <dayan.freitas@catolicasc.org.br>
2021-10-28 23:59:11 -0300

    setup

--- test/display_test.c
+++ test/display_test.c
@@ -5,11 +5,17 @@ void setUp(){};
 void tearDown(){};
 
 void test_funcao(void) {
-    TEST_ASSERT_EQUAL(1, funcao());
+    TEST_ASSERT_EQUAL_STRING("HELLO", "HELLO");
+
+}
+
+void test_funcao2(void) {
+    TEST_ASSERT_MESSAGE(2==2, "Ertto");
 }
 
 int main (void) {
     UNITY_BEGIN();
     RUN_TEST(test_funcao);
+    RUN_TEST(test_funcao2);
     return UNITY_END();
 }
